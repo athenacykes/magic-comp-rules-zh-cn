04b68726c92890af8fa3bcbb4f28273c29504863
HeliumOctahelide <dx3906@mail.ustc.edu.cn>
2024-03-05 22:49:22 +0800

    update rn

---
+++ scripts/get_rn_cell.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+iconName=$1
+displayText=$2
+echo "<img src='https://raw.githubusercontent.com/andrewgioia/keyrune/master/svg/${iconName}.svg' width='14' height='14' />[${displayText}](release_notes/faq_${iconName}.pdf)"
